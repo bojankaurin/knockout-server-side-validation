662519ec73d8b675db341b95f14448279d44c17e
Bojan Kaurin <bojankaurin@gmail.com>
2013-07-18 11:59:16 +0200

    Options can be customized.

--- src/knockout-server-side-validation.js
+++ src/knockout-server-side-validation.js
@@ -1,11 +1,12 @@
 /*
 ===============================================================================
-    Author:     Bojan Kaurin - @bojankaurin                              
-    License:    MIT (http://opensource.org/licenses/mit-license.php)           
+Author:     Bojan Kaurin - @bojankaurin                              
+License:    MIT (http://opensource.org/licenses/mit-license.php)           
                                                                                
-    Description: Validation of errors on server with KnockoutJS                             
+Description: Validation of errors on server with KnockoutJS                             
 ===============================================================================
 */
+if (typeof (ko) === undefined) { throw 'Knockout is required, please ensure it is loaded before loading this server side validation plug-in'; }
 
 //Guid helper
 (function (koValidate) {
@@ -28,7 +29,51 @@
     }
     $(document).ready(init);
     return self;
-}(ko || {}));
+} (ko || {}));
+
+//Options
+(function (koValidate) {
+    var self = koValidate;
+    self.serverSideValidator = self.serverSideValidator || {};
+
+    var defaults = {
+        bindings: [],
+        //Name of unique attribute name to add to each observable
+        uniqueAttributeName: "uniqueid",
+        //This attribute will be added to element on GUI with guid associated with it
+        dataValidateUniqueAttribute: "data-validate-uniqueid",
+        //Class to be added to input on validation error
+        inputValidationErrorClass: "ko-input-validation-error",
+        //Class to be added to message container on validation error
+        fieldValidationErrorClass: "ko-field-validation-error"
+    };
+
+    // make a copy  so we can use 'reset' later
+    var options = ko.utils.extend({}, defaults);
+
+    self.serverSideValidator.getConfigOptions = function () {
+        return options;
+    };
+
+    self.serverSideValidator.resetConfigOptions = function () {
+        options = jQuery.extend(options, defaults);
+    };
+
+    self.serverSideValidator.init = function(opt) {
+        opt = opt || {};
+        if (opt.bindings && !(opt.bindings instanceof Array)) {
+            throw "options.bindings must be Array";
+        }
+        ko.utils.extend(options, opt);
+    };
+    
+    function init() {
+        
+    }
+    
+    $(document).ready(init);
+    return self;
+} (ko || {}));
 
 //Validate
 (function (koValidate) {
@@ -36,25 +81,29 @@
     self.serverSideValidator = self.serverSideValidator || {};
     self.serverSideValidator.showValidationMessageHandler = null;
 
-    //Name of field to add to each observable
-    var uniqueIdField = "uniqueid";
-    //This attribute will be added to element on GUI with guid associated with it
-    var dataValidateUniqueAttribute = "data-validate-uniqueid";
-   
     //Regex to find attr with value inside data-bind.
     //For example inside data-bind="value:Name, attr : { class = 'class' }", should return attr : { class = 'class' }
     var regexForAttr = /attr\s*?:\s*?\{.+?\}/;
-
+    var bindingRegexPrefix = "\\s*?:\\s*?.+?(?=\\s|,|$)";
+    var dataBind = "data-bind";
+    var regexUniqueid = new RegExp(self.serverSideValidator.getConfigOptions().uniqueAttributeName + bindingRegexPrefix);
     var regexesForPropertyBinded = [
-        //When binded with value
-        /value\s*?:\s*?.+?(?=\s|,|$)/,
-        //When binded with checked
-        /checked\s*?:\s*?.+?(?=\s|,|$)/
+    //When binded with value
+		new RegExp("value" + bindingRegexPrefix),
+    //When binded with checked
+		new RegExp("checked" + bindingRegexPrefix)
+    //value\s*?:\s*?.+?(?=\s|,|$)/,
+    //checked\s*?:\s*?.+?(?=\s|,|$)/
     ];
 
-    var regexUniqueid = /uniqueid\s*?:\s*?.+?(?=\s|,|$)/;
-    
-    var dataBind = "data-bind";
+    //Merge custom bindings with knockout default bindings value and checked
+    for (var i = 0; i < self.serverSideValidator.getConfigOptions().bindings.length; i++) {
+        var regex = new RegExp(bindingRegexPrefix + self.serverSideValidator.getConfigOptions().bindings[i]);
+        //If item is not already added to array (Prevent duplicates)
+        if ($.inArray(regex, regexesForPropertyBinded) == -1) {
+            regexesForPropertyBinded.push(regex);
+        }
+    }
 
     /*
     Update each element with data-bind attribute to add uniqueid.
@@ -62,21 +111,21 @@
     */
     function updateView() {
         //Go through each element with data-bind attribute
-        $("*[" + dataBind + "]").each(function(index, elem) {
+        $("*[" + dataBind + "]").each(function (index, elem) {
             var element = $(elem);
             //data-bind value
             var attrValue = element.attr(dataBind);
             var tmp;
             var name = null;
-            
+
             var uniqueidMatch = attrValue.match(regexUniqueid);
-            if (uniqueidMatch && $.trim(uniqueidMatch[0].split(":")[0]) == uniqueIdField) {
+            if (uniqueidMatch && $.trim(uniqueidMatch[0].split(":")[0]) == self.serverSideValidator.getConfigOptions().uniqueAttributeName) {
                 return;
             }
-            
+
             //Go trough regexes for finding binding viewmodel property name.
             //For example data-bind="value:Name" should find Name, or data-bind="checked:RememberMe, should find RememberMe
-            $(regexesForPropertyBinded).each(function(index, elem) {
+            $(regexesForPropertyBinded).each(function (index, elem) {
                 var valueMatch = attrValue.match(elem);
                 if (name == null) {
                     if (valueMatch) {
@@ -91,12 +140,12 @@
                 if (attrValue) {
                     var match = attrValue.match(regexForAttr);
                     //Renders ": Name.uniqueid() }"
-                    var appendValue = uniqueIdField + " : " + name + "." + uniqueIdField + "() }";
+                    var appendValue = self.serverSideValidator.getConfigOptions().uniqueAttributeName + " : " + name + "." + self.serverSideValidator.getConfigOptions().uniqueAttributeName + "() }";
                     //If we founded knockout attr value, then append to end
                     if (match) {
                         tmp = attrValue.replace(match[0], match[0].replace("}", " ," + appendValue));
                     }
-                        //Else if there is no knockout attr value, add 
+                    //Else if there is no knockout attr value, add 
                     else {
                         tmp = attrValue + ", attr : { " + appendValue;
                     }
@@ -107,12 +156,12 @@
             }
         });
     };
-    
+
     self.applyValidation = function (viewModel) {
         updateView();
         startTraverseKoModel(viewModel);
     };
-    
+
     self.applyBindingsWithValidation = function (viewModel, rootNode) {
         updateView();
         startTraverseKoModel(viewModel);
@@ -120,7 +169,7 @@
     };
 
     self.serverSideValidator.updateKoModel = startTraverseKoModel;
-    
+
     /*
     Validate model and generate span bellow item with class ko-validate-error. 
     If self.serverSideValidator.showValidationMessageHandler(elem, message) is set, then this handler is used to put appropriate messages.
@@ -130,8 +179,8 @@
     self.serverSideValidator.validateModel = function (viewModel, data, unhandledMessagesHandler) {
         var valid = data && !(data.KoValid == false);
         //
-        $("*[" + dataValidateUniqueAttribute + "]").remove();
-        $("*[uniqueid]").removeClass("ko-input-validation-error");
+        $("*[" + self.serverSideValidator.getConfigOptions().dataValidateUniqueAttribute + "]").remove();
+        $("*[" + self.serverSideValidator.getConfigOptions().uniqueAttributeName + "]").removeClass(self.serverSideValidator.getConfigOptions().inputValidationErrorClass);
         if (data && data.KoValid == false && data.ModelState instanceof Array) {
             //Traverse through view model js object, and foreach elementName(generated based on self.serverSideValidator.getElement)
             //that is equal to Key that is returned from server in object in format { KoValid, ModelState }, where ModelState is array of errors,
@@ -141,18 +190,18 @@
                 $.each(data.ModelState, function (index, element) {
                     if (element.Key == elementName) {
                         try {
-                            var id = eval("viewModel." + elementName.replace(/\[/g, "()[") + "." + uniqueIdField + "()");
+                            var id = eval("viewModel." + elementName.replace(/\[/g, "()[") + "." + self.serverSideValidator.getConfigOptions().uniqueAttributeName + "()");
                         }
-                        catch(ex){
-                            if (unhandledMessagesHandler && typeof(unhandledMessagesHandler) == "function") {
+                        catch (ex) {
+                            if (unhandledMessagesHandler && typeof (unhandledMessagesHandler) == "function") {
                                 unhandledMessagesHandler(element.Key, element.Value);
                             }
                             return;
                         }
-                        var elem = $("*[" + uniqueIdField + "=" + id + "]");
-                        var message = '<span class="ko-field-validation-error" ' + dataValidateUniqueAttribute + '="' + id + '">' + element.Value + '</span>';
-                        if (typeof(element.Value) == "string" && element.Value.length) {
-                            elem.addClass("ko-input-validation-error");
+                        var elem = $("*[" + self.serverSideValidator.getConfigOptions().uniqueAttributeName + "=" + id + "]");
+                        var message = '<span class="' + self.serverSideValidator.getConfigOptions().fieldValidationErrorClass + '" ' + self.serverSideValidator.getConfigOptions().dataValidateUniqueAttribute + '="' + id + '">' + element.Value + '</span>';
+                        if (typeof (element.Value) == "string" && element.Value.length) {
+                            elem.addClass(self.serverSideValidator.getConfigOptions().inputValidationErrorClass);
                         }
                         if (self.serverSideValidator.showValidationMessageHandler && typeof (self.serverSideValidator.showValidationMessageHandler) == "function") {
                             self.serverSideValidator.showValidationMessageHandler(elem, message);
@@ -181,21 +230,21 @@
     }
     $(document).ready(init);
     return self;
-}(ko || {}));
+} (ko || {}));
 
 
 //Traverse stuff
 (function (koValidate) {
     var self = koValidate;
     self.serverSideValidator = self.serverSideValidator || {};
-    
+
     /*
     Set unique id (guid) to observable property,
     so each property can be identified on gui (each GUI data-binded element will have uniqueid on that property, so it will be bounded)
     */
     self.serverSideValidator.setUniqueId = function (object) {
-        if (object && !ko.isObservable(object.uniqueid)) {
-            object.uniqueid = ko.observable(self.serverSideValidator.guid());
+        if (object && !ko.isObservable(object[self.serverSideValidator.getConfigOptions().uniqueAttributeName])) {
+            object[self.serverSideValidator.getConfigOptions().uniqueAttributeName] = ko.observable(self.serverSideValidator.guid());
         }
     };
 
@@ -245,7 +294,7 @@
     }
     $(document).ready(init);
     return self;
-}(ko || {}));
+} (ko || {}));
 
 
 //MVC way of generating field names
@@ -258,24 +307,24 @@
     For MVC, if there is object Company with list of Emplyees, where each employee have name.
     Then, getElement should return Company.Emplyees[index].Name, where index is numeric index of employee in list.  
     */
-    self.serverSideValidator.getElement = function (parent, j) { 
-        var sufix = ""; 
-        if (parent != undefined && parent != null) { 
-            if (parent.endsWith("]")) sufix = "."; 
-            if (!isNaN(j)) { 
-                return parent + sufix + "[" + j + "]"; 
+    self.serverSideValidator.getElement = function (parent, j) {
+        var sufix = "";
+        if (parent != undefined && parent != null) {
+            if (parent.endsWith("]")) sufix = ".";
+            if (!isNaN(j)) {
+                return parent + sufix + "[" + j + "]";
             } else {
                 //For case when parent object exists but it is not an array
                 if (parent != undefined) sufix = ".";
-                return parent + sufix + j; 
-            } 
-        } 
-        return j; 
+                return parent + sufix + j;
+            }
+        }
+        return j;
     };
 
     function init() {
     }
     $(document).ready(init);
     return self;
-}(ko || {}));
+} (ko || {}));
 
